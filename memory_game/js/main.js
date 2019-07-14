1a7e32af8aa0f079089a44db975969b8846e2c1a
Walter Porzel <walterporzel@Walters-MacBook-Pro.local>
2019-07-14 15:06:30 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,23 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry, try again.");
-	}
-
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			console.log("You found a match!");
+		}
+		else{
+			console.log("Sorry, try again.");
+		}
+	}	
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+function flipCard(cardId){
+	console.log("User Fliped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
